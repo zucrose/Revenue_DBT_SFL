71e6426bc1ed8c87a12ada51ddec8bdad4d11b58
Uzair Khan <uzrvanced@gmail.com>
2026-04-02 06:23:37 +0000

    created audittable

---
+++ macros/auditlog.sql
@@ -0,0 +1,9 @@
+{%  macro auditlog(status='start',name='Default') %}
+{% if status =='start' %}
+insert into revenue_fact_dev.audit_log values (revenue_fact_dev.audit_seq_no.nextval,'{{name}}','{{status}}',current_timestamp(),NULL)
+{% endif %}
+{% if status=='Completed' %}
+update revenue_fact_dev.audit_log  set status='{{status}}' ,load_end=current_timestamp() where model_name='{{name}}'
+{% endif %}
+
+{% endmacro %}
--- models/bronze_revenue.sql
+++ models/bronze_revenue.sql
@@ -1,4 +1,6 @@
 {{config(
-    materialized='table'
+    materialized='table',
+    pre_hook="{{auditlog('start',this.name)}}",
+    post_hook="{{auditlog('Completed',this.name)}}"
 )}}
 select *,current_timestamp() as ingestion_time  from {{  source('SOURCE','revenue_data_raw') }}
