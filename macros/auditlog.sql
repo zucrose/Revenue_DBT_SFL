6846d02fc7ad4410adee21a0e7d91eb51a16683e
Uzair Khan <uzrvanced@gmail.com>
2026-04-02 07:39:11 +0000

    changing silver to incremental

--- macros/auditlog.sql
+++ macros/auditlog.sql
@@ -3,7 +3,8 @@
 insert into revenue_fact_dev.audit_log values (revenue_fact_dev.audit_seq_no.nextval,'{{name}}','{{status}}',current_timestamp(),NULL)
 {% endif %}
 {% if status=='Completed' %}
-update revenue_fact_dev.audit_log  set status='{{status}}' ,load_end=current_timestamp() where model_name='{{name}}'
+update revenue_fact_dev.audit_log  set status='{{status}}' ,load_end=current_timestamp() where
+ model_name='{{name}}' and load_end is null
 {% endif %}
 
 {% endmacro %}
--- models/silver_revenue.sql
+++ models/silver_revenue.sql
@@ -1,4 +1,10 @@
 {{config(
-    materialized='table'
+    materialized='incremental',
+    pre_hook=auditlog('start',this.name),
+    post_hook=auditlog('Completed',this.name)
 )}}
-select * from {{ ref('bronze_revenue')}}
+select * from {{ ref('bronze_revenue')}} 
+{%  if is_incremental() %}
+where event_time>( select coalesce(max(event_time),'1900-01-01 00:00:00'::timestamp) from {{this}})
+
+{% endif %}
